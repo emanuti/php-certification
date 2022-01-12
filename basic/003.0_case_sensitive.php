cdb91f479b3a2ac8388d66052869e96c03706b37
Emmanuel de Carvalho Garcia <emanu.ti@gmail.com>
2022-01-12 09:59:23 -0300

    Update 003.0_case_sensitive.php

--- basic/003.0_case_sensitive.php
+++ basic/003.0_case_sensitive.php
@@ -3,6 +3,10 @@
 // variables are case sensitive
 $variable = 'variable';
 
+// if you try to call uppercase
+echo $VARIABLE;
+
+// results in:
 /*
 PHP Notice:  Undefined variable: VARIABLE in /Users/emanuti/Sites/php-certification/003_case_sensitive.php on line 7
 PHP Stack trace:
@@ -13,6 +17,4 @@ Notice: Undefined variable: VARIABLE in /Users/emanuti/Sites/php-certification/0
 Call Stack:
 0.0029     388480   1. {main}() /Users/emanuti/Sites/php-certification/003_case_sensitive.php:0
  */
-
-echo $VARIABLE;
 ?>
